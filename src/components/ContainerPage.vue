6f4f50b850747758b26b127f42f9a3f5987ba00f
leegyuseung <lgs4002@naver.com>
2022-10-09 23:09:22 +0900

    레이아웃만들기& props를 통한 데이터바인딩

---
+++ src/components/ContainerPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <PostPage :datas="data" v-for="(data, i) in datas" :key="i" />
+  </div>
+</template>
+
+<script>
+import PostPage from "./PostPage.vue";
+export default {
+  components: { PostPage },
+  props: { datas: Array },
+  name: "ContainerPage",
+};
+</script>
+
+<style></style>
---
+++ src/components/PostPage.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="post">
+    <div class="post-header">
+      <div
+        class="profile"
+        :style="{ backgroundImage: `url(${datas.postImage})` }"
+      ></div>
+      <span class="profile-name">{{ datas.name }}</span>
+    </div>
+    <div
+      class="post-body"
+      :style="{ backgroundImage: `url(${datas.postImage})` }"
+    ></div>
+    <div class="post-content">
+      <p>{{ datas.likes }} Likes</p>
+      <p>
+        <strong>{{ datas.name }}</strong> {{ datas.content }}
+      </p>
+      <p class="date">{{ datas.date }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default { props: { datas: Object } };
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  background-image: url("https://placeimg.com/100/100/arch");
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  background-image: url("https://placeimg.com/640/480/animals");
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+}
+</style>
